61daa3f4351cc50cdbd8f87c96889964c633ae8a
James Burns <jrburns96@aol.com>
2022-02-01 20:29:39 -0500

    initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+// ^ idk that you need this.
+
+// tailwind is working!! :)
+
+function App() {
+  return (
+    <div> 
+      <button className="mt-8 px-4 py-2 border border-transparent text-l font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Select Wallet</button>
+    </div>
+  );
+}
+
+export default App;
